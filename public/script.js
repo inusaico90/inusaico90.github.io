9efe7776407c3eee7b6626f90ced30a871cf27f8
inusaico90 <erneyjj@hotmail.es>
2024-11-08 06:10:58 -0500

    Update script.js4

--- public/script.js
+++ public/script.js
@@ -1,47 +1,27 @@
-const API_URL_VERIFICAR = 'https://script.google.com/macros/s/AKfycbztCYgpqs7ifD0Eie_8y74CpNi7t-MMvm9SaR6SN334DBxFGeDU17Mrs1ohy3krttIH/exec';
+const API_URL_VERIFICAR = 'https://script.google.com/macros/s/AKfycbxd2Trr3JhZDytDGRctGZLVbl32QtT-HYWoL-Uu6eEnkRwiAME_vn5XHZrfLQdG2nEy0w/exec';
 
 function verificarCodigo() {
     const codigo = document.getElementById('codigo').value;
-    fetch(API_URL_VERIFICAR, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: codigo, accion: "verificar" })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.verificar) {
-            document.getElementById('resultado').innerText = "Código válido. Procediendo a actualizar...";
-            actualizarCodigo(codigo);
-        } else {
-            document.getElementById('resultado').innerText = "Código no válido o ya usado.";
-        }
-    })
-    .catch(error => {
-        console.error("Error en verificarCodigo:", error);
-        document.getElementById('resultado').innerText = "Error al verificar el código.";
-    });
+    const url = `${API_URL_VERIFICAR}?callback=callbackFunc&id=${codigo}&accion=verificar`;
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
 }
 
 function actualizarCodigo(codigo) {
-    fetch(API_URL_VERIFICAR, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ id: codigo, accion: "actualizar" })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.actualizado) {
-            document.getElementById('resultado').innerText = "Código actualizado exitosamente.";
-        } else {
-            document.getElementById('resultado').innerText = "Error al actualizar el código.";
-        }
-    })
-    .catch(error => {
-        console.error("Error en actualizarCodigo:", error);
-        document.getElementById('resultado').innerText = "Error al actualizar el código.";
-    });
+    const url = `${API_URL_VERIFICAR}?callback=callbackFunc&id=${codigo}&accion=actualizar`;
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+}
+
+function callbackFunc(data) {
+    if (data.verificar) {
+        document.getElementById('resultado').innerText = "Código válido. Procediendo a actualizar...";
+        actualizarCodigo(data.id);
+    } else if (data.actualizado) {
+        document.getElementById('resultado').innerText = "Código actualizado exitosamente.";
+    } else {
+        document.getElementById('resultado').innerText = "Código no válido o ya usado.";
+    }
 }
