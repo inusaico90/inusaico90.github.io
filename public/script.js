46aa18e167483fb995e879a4d557cf1859c50bc9
inusaico90 <erneyjj@hotmail.es>
2024-11-08 06:25:13 -0500

    Update script.js
    Debug

--- public/script.js
+++ public/script.js
@@ -2,20 +2,25 @@ const API_URL_VERIFICAR = 'https://script.google.com/macros/s/AKfycbxd2Trr3JhZDy
 
 function verificarCodigo() {
     const codigo = document.getElementById('codigo').value;
+    console.log("Código a verificar:", codigo); // Registro de depuración
     const url = `${API_URL_VERIFICAR}?callback=callbackFunc&id=${codigo}&accion=verificar`;
+    console.log("URL de verificación:", url); // Registro de depuración
     const script = document.createElement('script');
     script.src = url;
     document.body.appendChild(script);
 }
 
 function actualizarCodigo(codigo) {
+    console.log("Código a actualizar:", codigo); // Registro de depuración
     const url = `${API_URL_VERIFICAR}?callback=callbackFunc&id=${codigo}&accion=actualizar`;
+    console.log("URL de actualización:", url); // Registro de depuración
     const script = document.createElement('script');
     script.src = url;
     document.body.appendChild(script);
 }
 
 function callbackFunc(data) {
+    console.log("Datos recibidos:", data); // Registro de depuración
     if (data.verificar) {
         document.getElementById('resultado').innerText = "Código válido. Procediendo a actualizar...";
         actualizarCodigo(data.id);
